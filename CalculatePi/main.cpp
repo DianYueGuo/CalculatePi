c47e6c647d795d3a2b60d69017cdb05fb864d0f4
郭典約 <gdy940423@gmail.com>
2020-12-13 00:34:15 +0800

    Initial Commit

---
+++ CalculatePi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CalculatePi
+//
+//  Created by 郭典約 on 2020/12/13.
+//  Copyright © 2020 郭典約. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
